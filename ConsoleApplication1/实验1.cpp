6c532434952d5eb76d0bc62193c3102a0b92ae57
hunterZhu <798166809@qq.com>
2018-06-05 23:12:29 +0800

    添加项目文件。

---
+++ ConsoleApplication1/实验1.cpp
@@ -0,0 +1,92 @@
+#include "stdio.h"
+#include "stdlib.h"
+int k, k1;
+int sum;
+int coun = 1;
+int maxsum = 0;
+int ans1, ans2;
+typedef int datatype;
+typedef struct node
+{
+	datatype data;
+	datatype time;
+	struct node *next;
+}Linknode, *Link;
+
+Link Createlist()
+{
+	int a;
+	Link H, p, r;
+	H = (Link)malloc(sizeof(Linknode));
+	r = H;
+	
+	while (scanf("%d", &a) && a != '#')
+	{
+		p = (Link)malloc(sizeof(Linknode));
+		p->data = a;
+		p->time = coun;
+		coun++;
+		r->next = p;
+		r = p;
+	}
+	r->next = NULL;
+	return H;
+}
+
+void Adjmax(Link H)
+{
+	
+	Link p, p1, q, r, s, t;
+	p = H->next;
+	if ((p == NULL) || (coun - 1<k))
+		return;
+	while (p)
+	{
+		k1 = k;
+		sum = 0;
+		p1 = p;
+		while ((k1--) && (p1))
+		{
+			sum += p1->data;
+			q = p1->next;
+			p1 = q;
+		}
+		if (sum>maxsum)
+		{
+			ans1 = p->time;
+			ans2 = p->data;
+			maxsum = sum;
+		}
+		p = p->next;
+	}
+	printf("order:%d\ndata:%d\nmaxsum:%d",ans1, ans2, maxsum);
+	maxsum = 0;
+	ans1 = 0;
+	ans2 = 0;
+	coun = 1;
+	return;
+}
+void main()
+{
+	Link p,q;
+	int b;
+	do
+	{
+		printf("input k:\n");
+		scanf("%d", &k);
+		printf("Please input numbers:\n");
+		p = Createlist();
+		Adjmax(p);
+		while (p->next)
+		{
+			q = p;
+			p = p->next;
+			free(q);
+		}
+		free(p);
+		printf("\nÊÇ·ñ¼ÌÐø£¿ÊÇ£º1£»·ñ£º0\n");
+		getchar();
+		scanf("%d", &b);
+		
+	} while (b);
+}
